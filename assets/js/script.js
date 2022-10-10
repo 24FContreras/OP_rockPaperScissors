f38e510a19748dc5da5788a31ef6ea2b42ac0b99
Franco Contreras <24.f.contreras@gmail.com>
2022-10-10 16:33:11 -0300

    Rock paper scissors Ver.2 -refactored code to work with new interface -added new UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,63 @@
 console.log("🦆 Linkin' duck");
 
+//DOM DISPLAYS
+const playerScoreDigit = document.querySelector("#playerScore");
+const cpuScoreDigit = document.querySelector("#cpuScore");
+const roundResultDisplay = document.querySelector("#roundResultDisplay");
+const buttons = document.querySelectorAll(".buttons button");
+
+//GAME LOGIC
 let playerScore = 0;
 let cpuScore = 0;
 
-getComputerChoice = () => {
+const getComputerChoice = () => {
   let computerChoice;
   let randomNumber = Math.floor(Math.random() * (4 - 1) + 1);
 
   if (randomNumber === 1) {
-    computerChoice = "rock";
+    computerChoice = "Rock";
   } else if (randomNumber === 2) {
-    computerChoice = "paper";
+    computerChoice = "Paper";
   } else if (randomNumber === 3) {
-    computerChoice = "scissors";
+    computerChoice = "Scissors";
   }
   return computerChoice;
 };
 
-let playRound = (playerSelection, computerSelection) => {
-  computerSelection = getComputerChoice();
-  playerSelection = prompt("pick your weapon").toLocaleLowerCase();
+const playRound = (player, cpu) => {
+  let finalResult = player + cpu;
 
-  let resultString = playerSelection + computerSelection;
-  if (playerSelection === computerSelection) {
-    console.log("it's a tie");
-  } else if (
-    resultString === "rockscissors" ||
-    resultString === "paperrock" ||
-    resultString === "scissorspaper"
+  if (player === cpu) roundResultDisplay.textContent = "It's a tie";
+  else if (
+    finalResult === "RockScissors" ||
+    finalResult === "ScissorsPaper" ||
+    finalResult === "PaperRock"
   ) {
-    console.log("You win! " + playerSelection + " beats " + computerSelection);
+    roundResultDisplay.textContent = `You win! ${player} defeats ${cpu}`;
     playerScore++;
+    playerScoreDigit.textContent = playerScore;
   } else {
-    console.log("You lose! " + computerSelection + " beats " + playerSelection);
+    roundResultDisplay.textContent = `You lost! ${cpu} defeats ${player}`;
     cpuScore++;
+    cpuScoreDigit.textContent = cpuScore;
   }
 };
 
-game = () => {
-  for (let i = 0; playerScore < 5 && cpuScore < 5; i++) {
-    playRound();
-  }
+const game = (player, cpu) => {
+  if (playerScore < 5 && cpuScore < 5) playRound(player, cpu);
 
   if (playerScore === 5) {
-    console.log("You won the game!");
-  } else {
-    console.log("You lose the game");
+    roundResultDisplay.textContent = "Congratulations! You won the game"
+    return;
+  } else if (cpuScore === 5) {
+    roundResultDisplay.textContent = "You lost! Wanna try again?"
+    return;
   }
 };
 
-game();
+//ACTIVATING THE GAME
+for (const button of buttons) {
+  button.addEventListener("click", function () {
+    game(button.value, getComputerChoice());
+  });
+}
